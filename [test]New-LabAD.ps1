bfc3829345f593316cb8fc2f17b4bf8605c3d6dc
TimHighland <t.hoogland@hotmail.com>
2017-12-12 13:57:58 +0100

    new file

---
+++ [test]New-LabAD.ps1
@@ -0,0 +1,56 @@
+﻿function New-LabAD ($DomainName,$DomainExtension) 
+{
+    $FullName = $DomainName + "." + $DomainExtension
+    $FirstUserPassword = Read-Host "Password:" -AsSecureString
+    
+    function New-ADOU ($OUName)
+    {
+        $ObjectParameters = @{
+            Path = "OU=$FullName,DC=$DomainName,DC=$DomainExtension"
+            Name = $OUName
+            ProtectedFromAccidentalDeletion = $false
+        }
+        New-ADOrganizationalUnit @ObjectParameters
+    }
+
+    $NewADOU1 = @{
+        Name = $FullName
+        ProtectedFromAccidentalDeletion = $false
+    }   
+    $NewADOU2 = @{
+        Path = "OU=$FullName,DC=$DomainName,DC=$DomainExtension"
+        Name = 'Users'
+        ProtectedFromAccidentalDeletion = $false
+    }
+    $NewADOU3 = @{
+        Path = "OU=$FullName,DC=$DomainName,DC=$DomainExtension"
+        Name = 'Computers'
+        ProtectedFromAccidentalDeletion = $false
+    }
+    $NewADOU4
+
+    New-ADOrganizationalUnit @NewADOU1
+    New-ADOrganizationalUnit @NewADOU2
+    New-ADOrganizationalUnit @NewADOU3
+
+    New-ADOrganizationalUnit `
+        -Path "OU=$FullName,DC=$DomainName,DC=$DomainExtension" `
+        -Name 'Servers' `
+        -ProtectedFromAccidentalDeletion $false
+    New-ADOrganizationalUnit `
+        -Path "OU=$FullName,DC=$DomainName,DC=$DomainExtension" `
+        -Name 'Groups' `
+        -ProtectedFromAccidentalDeletion $false
+    New-ADUser `
+        -SamAccountName "t.hoogland" `
+        -DisplayName "Tim Hoogland" `
+        -Name "Tim" `
+        -Surname "Hoogland" `
+        -UserPrincipalName "t.hoogland" `
+        -AccountPassword $FirstUserPassword `
+        -Path "OU=Users,OU=$FullName,DC=$DomainName,DC=$DomainExtension" `
+        -Enabled $true
+    Add-AdGroupMember `
+        -Identity "Domain Admins" `
+        -Members "t.hoogland"
+}
